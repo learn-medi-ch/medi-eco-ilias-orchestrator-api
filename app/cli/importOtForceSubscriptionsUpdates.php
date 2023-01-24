ebece67d387e8d0830e166f356a8d851714f0706
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2023-01-24 18:25:00 +0100

    added cli

---
+++ app/cli/importOtForceSubscriptionsUpdates.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . "/../autoload.php";
+
+use MediEco\IliasUserOrchestratorOrbital\Adapters\Api\CliApi;
+use MediEco\IliasUserOrchestratorOrbital\Core\Ports\Messages;
+use MediEco\IliasUserOrchestratorOrbital\Core\Domain\ValueObjects;
+
+$cliApi = CliApi::new();
+
+$cliApi->importUsers(Messages\ImportUsers::new(
+    ValueObjects\FacultyId::OT,
+    ValueObjects\ImportType::FORCE_SUBSCRIPTIONS_UPDATES,
+));
